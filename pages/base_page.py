40cf8db663eff3773447e74d2aa6ae0a8d95c9d2
romgover <romgover@gmail.com>
2023-06-01 11:46:45 +0300

    Create new function (should_be_login_page)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
 
     # конструктор, метод который вызывается, когда мы создаем объект
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     # метод, который должен вручную открывать страницу
     def open(self):
         self.browser.get(self.url)
+
+    # метод, для неявного ожидания со значением по умолчанию - 10
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,7 @@
+import time
+
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,3 +10,10 @@ def test_guest_can_go_to_login_page(browser):
                                     # драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    time.sleep(5)
+    page.should_be_login_link()
+
+    link_reg_auth = "http://selenium1py.pythonanywhere.com/ru/accounts/login"
+    page_reg_auth = LoginPage(browser, link_reg_auth)
+    page_reg_auth.should_be_login_page()
+
